3b436505e1f46ebe05c12505ffe27542bc91525f
zsxh <bnbvbchen@gmail.com>
2020-02-15 17:03:40 +0800

    Disable `Ctrl-S` and `Ctrl-Q`
    `Ctrl-S` stops data from being transmitted to the terminal until you
    press `Ctrl-Q`.
    
    Now `Ctrl-S` can be read as a =19= byte and `Ctrl-Q` can be read as a
    =17= byte.

--- zsxh.c
+++ zsxh.c
@@ -15,12 +15,13 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
+  raw.c_iflag &= ~(IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main(int argc, char *argv[]) {
+int main() {
   enableRawMode();
 
   char c;
