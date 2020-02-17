77a9c552706d77f6506778c30f0b63246157de3e
zsxh <bnbvbchen@gmail.com>
2020-02-17 16:37:05 +0800

    Global state
    Our next goal is to get the size of the terminal, so we know how many
    rows to draw in editorDrawRows(). But first, let’s set up a global
    struct that will contain our editor state, which we’ll use to store
    the width and height of the terminal. For now, let’s just put our
    orig_termios global into the struct.

--- zsxh.c
+++ zsxh.c
@@ -11,8 +11,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
+struct editorConfig {
+  struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
+
 
 /*** terminal ***/
 
@@ -25,16 +29,16 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
     die("tcsetattr");
   }
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
