f946ef426a29631febdc6d88871515eba46d529c
zsxh <bnbvbchen@gmail.com>
2020-02-18 15:07:18 +0800

    Welcome message
    Display a welcome message in the middle of the screen.

--- zsxh.c
+++ zsxh.c
@@ -10,6 +10,9 @@
 #include <unistd.h>
 
 /*** defines ***/
+
+#define ZSXH_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -120,7 +123,21 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "Zxsh editor -- version %s", ZSXH_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
