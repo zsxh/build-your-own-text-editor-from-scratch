b1631caa77fd152d70d48ca67b221b7533785c6f
zsxh <bnbvbchen@gmail.com>
2020-02-26 10:47:38 +0800

    Colorful search results
    add HL_MATCH to the editorHighlight enum, and map it to the color
    blue(34) in editorSyntazToColor().
    
    =match - row->render= is the index into =render= of the match, so we
    use that as our index into =hl=.

--- zsxh.c
+++ zsxh.c
@@ -40,7 +40,8 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
-  HL_NUMBER
+  HL_NUMBER,
+  HL_MATCH
 };
 
 /*** data ***/
@@ -207,6 +208,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntazToColor(int hl) {
   switch (hl) {
     case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
     default: return 37;
   }
 }
@@ -467,6 +469,8 @@ void editorFindCallback(char *query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
